5e1f7dc57b3144cb90592e1f3db1709fcba70e5a
AskarBekB <askarbekbergen30@gmail.com>
2023-05-12 20:10:36 +0600

    ASko

--- spotify/spotify/spotifyStarter/views.py
+++ spotify/spotify/spotifyStarter/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "spotify/home.html")
+    return render(request, "spotify/index.html")
